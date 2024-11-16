4fc35ba8d0aebd5d8cb58321827140d537a1dee1
sarthak <159271874+sarthak4115@users.noreply.github.com>
2024-11-16 23:23:37 +0530

    Update student management system.cpp

--- student management system.cpp
+++ student management system.cpp
@@ -1,112 +1,175 @@
-#include<iostream>
-#include<fstream>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 using namespace std;
 
-class temp{
-    string rollNum,name,fName,address,search;
+class StudentManager {
+    string rollNum, name, fName, address, search;
     fstream file;
-    public:
-    void addStu();
-    void viewStu();
-    void searchStu();
-}obj;
 
-int main(){
+public:
+    void addStudent();
+    void viewStudents();
+    void searchStudent();
+    void deleteStudent();
+    void displayMenu();
+};
 
-    char choice;
-    cout<<"---------------------------"<<endl;
-    cout<<"1- Add Student Record"<<endl;
-    cout<<"2- View All Student Record"<<endl;
-    cout<<"3- Search Student Record"<<endl;
-    cout<<"4- Exit"<<endl;
-    cout<<"---------------------------"<<endl;
-    cin>>choice;
+void StudentManager::addStudent() {
+    cout << "\n--- Add New Student Record ---\n";
+    cout << "Enter Student Roll Number: ";
+    getline(cin, rollNum);
+    cout << "Enter Student Name: ";
+    getline(cin, name);
+    cout << "Enter Student Father Name: ";
+    getline(cin, fName);
+    cout << "Enter Student Address: ";
+    getline(cin, address);
 
-    switch(choice){
-        case '1':
-            cin.ignore();
-            obj.addStu();
-        break;
-        case '2':
-            cin.ignore();
-            obj.viewStu();
-        break;
-        case '3':
-            cin.ignore();
-            obj.searchStu();
-        break;
-        case '4':
-            return 0;
-        break;
-        default:
-            cout<<"Invalid Choice...!";
+    file.open("stuData.txt", ios::out | ios::app);
+    if (file.is_open()) {
+        file << rollNum << "*" << name << "*" << fName << "*" << address << "\n";
+        file.close();
+        cout << "Student record added successfully!\n";
+    } else {
+        cout << "Error: Unable to open file for writing.\n";
     }
-
 }
 
-void temp :: addStu(){
-    
-    cout<<"Enter Student Roll Number :: ";
-    getline(cin,rollNum);
-    cout<<"Enter Student Name ::";
-    getline(cin,name);
-    cout<<"Enter Student Father Name ::";
-    getline(cin,fName);
-    cout<<"Enter Student Address ::";
-    getline(cin,address);
-
-    file.open("stuData.txt",ios :: out | ios :: app);
-    file<<rollNum<<"*";
-    file<<name<<"*";
-    file<<fName<<"*";
-    file<<address<<endl;
-    file.close();
-}
-
-void temp :: viewStu(){
-    
-    file.open("stuData.txt",ios :: in);
-    getline(file,rollNum,'*');
-    getline(file,name,'*');
-    getline(file,fName,'*');
-    getline(file,address,'\n');
-    while(!file.eof()){
-        cout<<"\n";
-        cout<<"Student Roll Number :: "<<rollNum<<endl;
-        cout<<"Student Name :: "<<name<<endl;
-        cout<<"Student Father Name :: "<<fName<<endl;
-        cout<<"Student Address :: "<<address<<endl;
-
-        getline(file,rollNum,'*');
-        getline(file,name,'*');
-        getline(file,fName,'*');
-        getline(file,address,'\n');
-    }
-    file.close();
-}
-
-void temp :: searchStu(){
-
-    cout<<"Enter Student Roll Number :: ";
-    getline(cin,search);
-    
-    file.open("stuData.txt",ios :: in);
-    getline(file,rollNum,'*');
-    getline(file,name,'*');
-    getline(file,fName,'*');
-    getline(file,address,'\n');
-    while(!file.eof()){
-        if(rollNum == search){
-            cout<<endl;
-            cout<<"Student Roll Number :: "<<rollNum<<endl;
-            cout<<"Student Name :: "<<name<<endl;
-            cout<<"Student Father Name :: "<<fName<<endl;
-            cout<<"Student Address :: "<<address<<endl;       
+void StudentManager::viewStudents() {
+    cout << "\n--- All Student Records ---\n";
+    file.open("stuData.txt", ios::in);
+    if (file.is_open()) {
+        bool recordsFound = false;
+        while (getline(file, rollNum, '*') &&
+               getline(file, name, '*') &&
+               getline(file, fName, '*') &&
+               getline(file, address)) {
+            recordsFound = true;
+            cout << "\nStudent Roll Number: " << rollNum
+                 << "\nStudent Name: " << name
+                 << "\nStudent Father Name: " << fName
+                 << "\nStudent Address: " << address << "\n";
         }
-        getline(file,rollNum,'*');
-        getline(file,name,'*');
-        getline(file,fName,'*');
-        getline(file,address,'\n');
+        file.close();
+        if (!recordsFound) {
+            cout << "No student records found.\n";
+        }
+    } else {
+        cout << "Error: Unable to open file for reading.\n";
     }
-    file.close();
+}
+
+void StudentManager::searchStudent() {
+    cout << "\n--- Search Student Record ---\n";
+    cout << "Enter Student Roll Number to Search: ";
+    getline(cin, search);
+
+    file.open("stuData.txt", ios::in);
+    if (file.is_open()) {
+        bool recordFound = false;
+        while (getline(file, rollNum, '*') &&
+               getline(file, name, '*') &&
+               getline(file, fName, '*') &&
+               getline(file, address)) {
+            if (rollNum == search) {
+                recordFound = true;
+                cout << "\nStudent Roll Number: " << rollNum
+                     << "\nStudent Name: " << name
+                     << "\nStudent Father Name: " << fName
+                     << "\nStudent Address: " << address << "\n";
+                break;
+            }
+        }
+        file.close();
+        if (!recordFound) {
+            cout << "No record found for Roll Number: " << search << "\n";
+        }
+    } else {
+        cout << "Error: Unable to open file for reading.\n";
+    }
+}
+
+void StudentManager::deleteStudent() {
+    cout << "\n--- Delete Student Record ---\n";
+    cout << "Enter Student Roll Number to Delete: ";
+    getline(cin, search);
+
+    vector<string> records;
+    bool recordFound = false;
+
+    file.open("stuData.txt", ios::in);
+    if (file.is_open()) {
+        while (getline(file, rollNum, '*') &&
+               getline(file, name, '*') &&
+               getline(file, fName, '*') &&
+               getline(file, address)) {
+            if (rollNum == search) {
+                recordFound = true;
+            } else {
+                records.push_back(rollNum + "*" + name + "*" + fName + "*" + address);
+            }
+        }
+        file.close();
+    } else {
+        cout << "Error: Unable to open file for reading.\n";
+        return;
+    }
+
+    if (recordFound) {
+        file.open("stuData.txt", ios::out | ios::trunc); // Clear the file
+        if (file.is_open()) {
+            for (const auto& record : records) {
+                file << record << "\n";
+            }
+            file.close();
+            cout << "Record deleted successfully!\n";
+        } else {
+            cout << "Error: Unable to open file for writing.\n";
+        }
+    } else {
+        cout << "No record found for Roll Number: " << search << "\n";
+    }
+}
+
+void StudentManager::displayMenu() {
+    char choice;
+    do {
+        cout << "\n--- Student Management System ---\n";
+        cout << "1 - Add Student Record\n";
+        cout << "2 - View All Student Records\n";
+        cout << "3 - Search Student Record\n";
+        cout << "4 - Delete Student Record\n";
+        cout << "5 - Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+        cin.ignore(); // Clear the input buffer
+
+        switch (choice) {
+            case '1':
+                addStudent();
+                break;
+            case '2':
+                viewStudents();
+                break;
+            case '3':
+                searchStudent();
+                break;
+            case '4':
+                deleteStudent();
+                break;
+            case '5':
+                cout << "Exiting the program. Goodbye!\n";
+                return;
+            default:
+                cout << "Invalid choice. Please try again.\n";
+        }
+    } while (choice != '5');
+}
+
+int main() {
+    StudentManager manager;
+    manager.displayMenu();
+    return 0;
 }
